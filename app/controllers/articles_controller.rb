d38a1585eda026f7805601cc24b3cc21a9e4d691
Jose Manuel Garcia Portillo <jgarciaportillo@gmail.com>
2021-05-18 15:39:44 +0900

    articles page

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,46 @@
+class ArticlesController < ApplicationController
+  #READ
+  def index
+    @articles = Article.all
+  end
+
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  #CREATE
+  def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(restaurant_params)
+    @article.save
+
+    redirect_to article_path(@article)
+  end
+  # UPDATE
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    @article.update(restaurant_params)
+
+    redirect_to article_path(@article)
+  end
+  # DELETE
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+
+    redirect_to article_path(@article)
+  end
+
+  private
+
+  def restaurant_params
+    params.require(:article).permit(:title, :content)
+  end
+end
